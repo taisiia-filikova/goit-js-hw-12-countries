4eb6cf65111d9f1fcff702e24377de5be7929fab
taisiia-filikova <filikova@gmail.com>
2020-11-06 21:10:11 +0200

    исправляет ошибку в пути

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,7 @@
 const BASE_URL = 'https://restcountries.eu/rest/v2/name';
 
-export default function fetchCountries(countrySearch) {
+function fetchCountries(countrySearch) {
   return fetch(`${BASE_URL}/${countrySearch}`).then(response => response.json());
 }
+
+export default { fetchCountries };
